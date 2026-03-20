f041b23607577c9c84b3987ae449fad2970a8302
sjieys <0wonyounglee0@gmail.com>
2026-03-20 20:12:56 +0900

    환경 설정용 커밋

--- app/src/main/java/com/example/umc_10th/MainActivity.kt
+++ app/src/main/java/com/example/umc_10th/MainActivity.kt
@@ -22,26 +22,5 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        binding.ivHappy.setOnClickListener {
-            binding.tvHappy.setTextColor(ContextCompat.getColor(this, R.color.happy));
-            binding.tvHappy.setBackgroundColor(ContextCompat.getColor(this, R.color.background_happy));
-        }
-
-        binding.ivExcited.setOnClickListener {
-            binding.tvExcited.setTextColor(ContextCompat.getColor(this, R.color.excited));
-            binding.tvExcited.setBackgroundColor(ContextCompat.getColor(this, R.color.background_excited));
-        }
-        binding.ivNormal.setOnClickListener {
-            binding.tvNormal.setTextColor(ContextCompat.getColor(this, R.color.normal));
-            binding.tvNormal.setBackgroundColor(ContextCompat.getColor(this, R.color.background_normal));
-        }
-        binding.ivSad.setOnClickListener {
-            binding.tvSad.setTextColor(ContextCompat.getColor(this, R.color.sad));
-            binding.tvSad.setBackgroundColor(ContextCompat.getColor(this, R.color.background_sad));
-        }
-        binding.ivAngry.setOnClickListener {
-            binding.tvAngry.setTextColor(ContextCompat.getColor(this, R.color.angry));
-            binding.tvAngry.setBackgroundColor(ContextCompat.getColor(this, R.color.background_angry));
-        }
     }
 }
