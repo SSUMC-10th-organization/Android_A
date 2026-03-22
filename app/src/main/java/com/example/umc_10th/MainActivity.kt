26394ff90f4cbbaaff7772978693a3d4c9b4daf5
ItzTree <kangjj17@naver.com>
2026-03-22 13:54:14 +0900

    [juni/#2] feat: BottomNav 및 Fragment 추가

--- app/src/main/java/com/example/umc_10th/MainActivity.kt
+++ app/src/main/java/com/example/umc_10th/MainActivity.kt
@@ -5,16 +5,45 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.umc_10th.fragment.CartFragment
+import com.example.umc_10th.fragment.HomeFragment
+import com.example.umc_10th.fragment.ProfileFragment
+import com.example.umc_10th.fragment.ShopFragment
+import com.example.umc_10th.fragment.WishlistFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        // 초기 화면
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, HomeFragment())
+            .commit()
+
+        bottomNav.setOnItemSelectedListener { item ->
+            val fragment = when (item.itemId) {
+                R.id.nav_home       -> HomeFragment()
+                R.id.nav_shop       -> ShopFragment()
+                R.id.nav_wishlist   -> WishlistFragment()
+                R.id.nav_cart       -> CartFragment()
+                R.id.nav_profile    -> ProfileFragment()
+                else -> return@setOnItemSelectedListener false
+            }
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit()
+            true
         }
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
