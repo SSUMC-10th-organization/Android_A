5d5fd7e86552a548c439124cfa1fc7b83b3aecb5
ItzTree <kangjj17@naver.com>
2026-03-22 16:33:21 +0900

    [juni/#2] feat: home fragment 완성

--- app/src/main/java/com/example/umc_10th/MainActivity.kt
+++ app/src/main/java/com/example/umc_10th/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.umc_10th
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.umc_10th.fragment.CartFragment
@@ -18,7 +21,31 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(0, systemBars.top, 0, 0)
+            insets
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottom_nav)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(0, 0, 0, systemBars.bottom)
+            insets
+        }
+
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val typeface = ResourcesCompat.getFont(this, R.font.noto_sans_regular)
+
+        for (i in 0 until bottomNav.menu.size()) {
+            val menuView = bottomNav.getChildAt(0) as ViewGroup
+            val itemView = menuView.getChildAt(i) as ViewGroup
+            for (j in 0 until itemView.childCount) {
+                val view = itemView.getChildAt(j)
+                if (view is TextView) {
+                    view.typeface = typeface
+                }
+            }
+        }
 
         // 초기 화면
         supportFragmentManager.beginTransaction()
@@ -39,11 +66,5 @@ class MainActivity : AppCompatActivity() {
                 .commit()
             true
         }
-
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
     }
 }
