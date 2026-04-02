38f6c3f4c880642b8faabf919926b55f8417ee6a
ID4372 <hwan4372@gmail.com>
2026-04-02 12:08:34 +0900

    위시리스트 하트 구현 완료

--- app/src/main/java/com/example/umc_10th/adapter/WishlistAdapter.kt
+++ app/src/main/java/com/example/umc_10th/adapter/WishlistAdapter.kt
@@ -3,6 +3,7 @@ package com.example.umc_10th.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.umc_10th.R
 import com.example.umc_10th.data.PurchaseProduct
 import com.example.umc_10th.databinding.ItemWishlistBinding
 
@@ -18,7 +19,6 @@ class WishlistAdapter(
             binding.productName.text = product.name
             binding.productExplain.text = product.explain
             binding.productPrice.text = product.price
-            // 하트 버튼이 없으니 관련 코드도 필요 없습니다!
         }
     }
 
@@ -27,10 +27,12 @@ class WishlistAdapter(
             LayoutInflater.from(parent.context), parent, false
         )
         return WishlistViewHolder(binding)
+
     }
 
     override fun onBindViewHolder(holder: WishlistViewHolder, position: Int) {
         holder.bind(itemList[position])
+        holder.binding.productHeartBtn.setImageResource(R.drawable.ic_favorite_heart)
     }
 
     override fun getItemCount(): Int = itemList.size
