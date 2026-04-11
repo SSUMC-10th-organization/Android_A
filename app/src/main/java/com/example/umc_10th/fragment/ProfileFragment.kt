b7921cb2144e98d32b41154330d84b8fb06a1b09
ItzTree <kangjj17@naver.com>
2026-04-12 01:27:54 +0900

    [juni/#5] feat: Profile Fragment 구현

--- app/src/main/java/com/example/umc_10th/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/umc_10th/fragment/ProfileFragment.kt
@@ -5,12 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.umc_10th.R
+import com.example.umc_10th.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
